6f9abac58a76cb7de08ae4219f3ca29290d34b39
FLC <paulespling@gmail.com>
2020-07-19 19:32:52 +0800

    Add enums for constans

---
+++ src/Mindnote/enums.js
@@ -0,0 +1,59 @@
+const EDGE = {
+  TOP: "top",
+  RIGHT: "right",
+  BOTTOM: "bottom",
+  LEFT: "left",
+};
+
+const CURVE_IN_OUT = {
+  IN: "in",
+  OUT: "out",
+};
+
+const CLOCKFACE = {
+  ONE: 1,
+  TWO: 2,
+  THREE: 3,
+  FOUR: 4,
+  FIVE: 5,
+  SIX: 6,
+  SEVEN: 7,
+  EIGHT: 8,
+  NINE: 9,
+  TEN: 10,
+  ELEVEN: 11,
+  TWELVE: 12,
+};
+
+const CURVE_CONTROL_TYPE = {
+  START_CONTROL: "startControl",
+  END_CONTROL: "endControl",
+};
+
+const CURVE_POINT_TYPE = {
+  START: "start",
+  END: "end",
+};
+
+const NODE_POINT_TYPE = {
+  TOP_LEFT: "topLeft",
+  TOP_RIGHT: "topRight",
+  BOTTOM_RIGHT: "bottomRight",
+  BOTTOM_LEFT: "bottomLeft",
+};
+
+const LIST_ACTION_TYPE = {
+  ADD_ITEMS: "ADD_ITEMS",
+  UPDATE_ITEMS: "UPDATE_ITEMS",
+  DELETE_ITEMS: "DELETE_ITEMS",
+};
+
+export {
+  LIST_ACTION_TYPE,
+  EDGE,
+  CURVE_IN_OUT,
+  CLOCKFACE,
+  CURVE_CONTROL_TYPE,
+  CURVE_POINT_TYPE,
+  NODE_POINT_TYPE,
+};
