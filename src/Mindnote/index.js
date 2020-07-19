e33c1a10938feaf997204880a8ad8b0fde9d4b29
FLC <paulespling@gmail.com>
2020-07-19 19:30:51 +0800

    Add function for manipulating nodeList and curveList

--- src/Mindnote/index.js
+++ src/Mindnote/index.js
@@ -3,7 +3,39 @@ import Header from "../Header";
 import Canvas from "./Canvas";
 import Tool from "./Tool";
 
+const listReducer = (list, action) => {
+  switch (action.type) {
+    case LIST_ACTION_TYPE.ADD_ITEMS:
+      return [...list, ...action.items];
+    case LIST_ACTION_TYPE.UPDATE_ITEMS:
+      const unChangedItems = list.filter(
+        (item) =>
+          !action.items.some(
+            (itemToBeUpdated) => itemToBeUpdated.id === item.id
+          )
+      );
+      return [...unChangedItems, ...action.items];
+    case LIST_ACTION_TYPE.DELETE_ITEMS:
+      return list.filter(
+        (item) =>
+          !action.items.some(
+            (itemToBeDeleted) => itemToBeDeleted.id === item.id
+          )
+      );
+    default:
+      return list;
+  }
+};
+
 const Mindnote = (props) => {
+  const [nodeList, dispatchNodes] = useReducer(listReducer, []);
+  const [curveList, dispatchCurves] = useReducer(listReducer, []);
+  const ItemContextValue = {
+    nodeList,
+    dispatchNodes,
+    curveList,
+    dispatchCurves,
+  };
   return (
     <div className="mindnote">
       <Header>
