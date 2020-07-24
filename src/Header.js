cca6f9326a27788f73a61115f746faec3febe11c
FLC <paulespling@gmail.com>
2020-07-24 13:15:11 +0800

    Set Home, Docs and Mindnote pages route

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,54 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 import "./Header.css";
-import { Link } from "react-router-dom";
+import {
+  Link,
+  NavLink,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+  useRouteMatch,
+  useHistory,
+} from "react-router-dom";
 
 const Header = (props) => {
+  const { path, url } = useRouteMatch();
+  let history = useHistory();
+
+  const homeNav = (
+    <nav className="navbar">
+      <div className="navbar-item">Feature</div>
+      <div className="navbar-item">Pricing</div>
+      <div className="navbar-item">Support</div>
+      <div className="navbar-item">About</div>
+    </nav>
+  );
+  const docsNav = (
+    <nav className="navbar">
+      <div className="navbar-item">
+        <NavLink to="/docs/public">Public Mindnotes</NavLink>
+      </div>
+      <div className="navbar-item">
+        <NavLink to="/docs/my">My Mindnotes</NavLink>
+      </div>
+    </nav>
+  );
+
+  const handleGoBack = () => history.goBack();
+  const mindnoteNav = (
+    <div className="go-back-btn" onClick={handleGoBack}>
+      <i className="fas fa-chevron-left"></i>&nbsp; Back
+    </div>
+  );
+
   return (
     <header className="header">
       <div className="title">
         <Link to="/">MindNote</Link>
       </div>
-      {props.children}
+      {path === "/home" && homeNav}
+      {path === "/docs" && docsNav}
+      {path === "/mindnote/:mindnoteId" && mindnoteNav}
       <div className="member-btn">
         <i className="fas fa-user-alt"></i>
       </div>
--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import "./Home.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Home = (props) => {
   return (
     <div className="home">
-      <Header>
-        <nav className="navbar">
-          <div className="navbar-item">Feature</div>
-          <div className="navbar-item">Pricing</div>
-          <div className="navbar-item">Support</div>
-          <div className="navbar-item">About</div>
-        </nav>
-      </Header>
       <main className="main">
         <h1 className="main-title">
           Mind Mapping &nbsp;<i className="fas fa-plus"></i>&nbsp; Note Taking
@@ -27,11 +19,11 @@ const Home = (props) => {
           </button>
           <span>&nbsp;or&nbsp;</span>
           <button className="main-btn sign-up-btn">
-            <Link to="/docs">Sign Up</Link>
+            <Link to="/docs/my">Sign Up</Link>
           </button>
           <div className="main-block">
             Already had mindnotes?&nbsp;
-            <Link className="link" to="./docs">
+            <Link className="link" to="/docs">
               Log in
             </Link>
             &nbsp;here.
