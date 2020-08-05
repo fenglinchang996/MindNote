2e81289bb1c78f2fe5a31b5d0be859f5df728c67
FLC <paulespling@gmail.com>
2020-08-05 10:48:19 +0800

    Redesign Langing page

--- src/home/index.js
+++ src/home/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Home.css";
-import mindMap from "./Mind-Map-Pic.png";
-import noteTaking from "./Note-Taking-Pic.png";
+import mindMapIcon from "./mind-map-icon.svg"; // https://www.iconfinder.com/icons/3209313/avatar_circuit_customer_digital_mind_icon
+import noteTakingIcon from "./note-taking-icon.svg"; // https://www.iconfinder.com/icons/4212916/education_note_notes_student_icon
+import mindMap from "./mind-map-pic.png";
+import noteTaking from "./note-taking-pic.png";
 import { Link } from "react-router-dom";
 
 const Home = (props) => {
@@ -9,22 +11,28 @@ const Home = (props) => {
     <div className="home">
       <main className="main">
         <div className="main-block">
-          <h1 className="main-title">
-            Mind Mapping &nbsp;<i className="fas fa-plus"></i>&nbsp; Note Taking
-          </h1>
-          <h3 className="main-desc">
-            Map Out Your Ideas Visually and Organize Them Detaily
-          </h3>
+          <div className="main-title">
+            <h1 className="title">Mind Mapping</h1>
+            <i className="fas fa-plus"></i>
+            <h1 className="title">Note Taking</h1>
+          </div>
+          <div className="main-desc">
+            <h3 className="desc">Map Out Your Ideas Visually</h3>
+            <h3 className="desc">and</h3>
+            <h3 className="desc">Organize Them Detaily</h3>
+          </div>
           <div className="main-content">
-            <button className="main-btn sign-up-btn">
-              <Link to="/member/signup">Try MindNote for Free</Link>
-            </button>
-            <button className="main-btn gallery-btn">
-              <Link to="/docs/public">
+            <div>
+              <Link className="main-btn sign-up-btn" to="/member/signup">
+                Try MindNote for Free
+              </Link>
+            </div>
+            <div>
+              <Link className="main-btn gallery-btn" to="/docs/public">
                 View People's Mindnotes &nbsp;
                 <i className="fas fa-chevron-right"></i>
               </Link>
-            </button>
+            </div>
           </div>
           <div className="main-content">
             Already had mindnotes?&nbsp;
@@ -33,20 +41,24 @@ const Home = (props) => {
             </Link>
             &nbsp;here.
           </div>
+          <img className="main-pic mind-map" src={mindMapIcon} />
+          <img className="main-pic note-taking" src={noteTakingIcon} />
         </div>
         <div className="second-block">
           <div className="second-desc">
+            <img className="icon" src={mindMapIcon} />
             <h3>Visualize Your Ideas with Mind Mapping</h3>
             <p>See your ideas in a whole picture effectively.</p>
           </div>
-          <img className="mind-map-pic" src={mindMap} />
+          <img className="pic" src={mindMap} />
         </div>
         <div className="second-block">
-          <img className="note-taking-pic" src={noteTaking} />
           <div className="second-desc">
+            <img className="icon" src={noteTakingIcon} />
             <h3>Orginize Your Thoughts with Note Taking</h3>
             <p>Write down everything without ignoring any details.</p>
           </div>
+          <img className="pic" src={noteTaking} />
         </div>
       </main>
     </div>
